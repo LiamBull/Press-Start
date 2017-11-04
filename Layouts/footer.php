83271f979692ed7ed79008b179baaca3634f4a48
Matthew Wierzbicki <mateuszwierzbicki97@gmail.com>
2017-11-04 14:56:58 -0400

    Fixed redunant code
    Took out dbcon script from bottom of page and threw into footer.

--- Layouts/footer.php
+++ Layouts/footer.php
@@ -1,3 +1,16 @@
+<script src="https://www.gstatic.com/firebasejs/4.6.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBl4h7SkK30JszfkAoYXsOXCgHBBlNjB_0",
+    authDomain: "dbase-6206-01.firebaseapp.com",
+    databaseURL: "https://dbase-6206-01.firebaseio.com",
+    projectId: "dbase-6206-01",
+    storageBucket: "dbase-6206-01.appspot.com",
+    messagingSenderId: "173142686947"
+  };
+  firebase.initializeApp(config);
+</script>
 <!-- </main> -->
 <script>
   // creates the cool more info 
@@ -9,14 +22,14 @@ $( "div.card" ).hover(
 );
 </script>
 
-         <!-- Bootstrap core JavaScript
-         ================================================== -->
-         <!-- Placed at the end of the document so the pages load faster -->
-         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-         <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-         <script src="../../../../assets/js/vendor/popper.min.js"></script>
-         <script src="../../../../dist/js/bootstrap.min.js"></script>
-         <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-         <script src="../../../../assets/js/ie10-viewport-bug-workaround.js"></script>
-       </body>
-       </html>
+ <!-- Bootstrap core JavaScript
+ ================================================== -->
+ <!-- Placed at the end of the document so the pages load faster -->
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+ <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+ <script src="../../../../assets/js/vendor/popper.min.js"></script>
+ <script src="../../../../dist/js/bootstrap.min.js"></script>
+ <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ <script src="../../../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
