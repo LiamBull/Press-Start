6823bfc85f86999f83d6b541b1a433a94097f3bc
JosephLees <joey_lees@hotmail.com>
2017-11-05 01:40:24 -0500

    added basic struc files
    just added the site progress we had so far

---
+++ Layouts/header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../../../favicon.ico">
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+  <title>GameBox Items</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+
+  <!-- Bootstrap core CSS -->
+  <link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="./css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Press Start</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
+  </div>
+</nav>
+
+
+  <div class="album">
+    <!-- 12 wide container -->
+    <div class="container">
---
+++ itemView.php
@@ -0,0 +1,108 @@
+<?php include('./Layouts/header.php'); ?>
+      <!-- ROW 1 YO-->
+      <div class="row">
+        <div class="col-md-3">
+          <h2>Last Of Us</h2>
+        </div>
+
+      </div>
+      <!-- ROW 2 YO-->
+      <div class="row">
+        <div class="col-md-12">
+          <h5>by Sony Computer Entertainment </h5>
+          <h6>Playstation 4</h6>
+        </div>
+      </div>
+
+      <!-- ROW 2 YO-->
+      <div class="row">
+        <div class="col-md-5 itemImageBox">
+          <img class="itemImage" src="http://psmedia.playstation.com/is/image/psmedia/the-last-of-us-remastered-two-column-01-ps4-us?$TwoColumn_Image$">
+        </div>
+        <!-- PRICES -->
+        <div class="col-md-7">
+          <div class="row">
+            <div class="col-md-6" style="background-color: purple;">
+              <div class="price">
+                <h5>NEW</h5>
+                $1159.99
+              </div>
+            </div>
+
+            <div class="col-md-6" style="background-color: orange;">
+              <div class="price">
+                <h5>RECYCLED</h5>
+                $39.99
+              </div>
+
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-3" style="background-color: yellow; ">
+              <div class="rating">
+                <img class="ratingImg" src="http://images2.wikia.nocookie.net/__cb20121105184161/nintendo/en/images/1/12/ESRB_M.png">
+              </div>
+            </div>
+
+            <div class="col-md-9" style="background-color: green; ">
+              <div class="genre">
+                <p>Genre</p>
+              </div>
+              <div class="genre">
+                <p>Action</p>
+              </div>
+              <div class="genre">
+                <p>Release Date</p>
+              </div>
+              <div class="genre">
+                <p>June 14, 2013</p>
+              </div>
+              <div class="genre">
+                <p>Item Code</p>
+              </div>
+              <div class="genre">
+                <p>JXJ123J12</p>
+              </div>
+
+            </div>
+
+
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="row">
+        <div class="col-md-12" style="padding: 3%;">
+          <h4 style="text-align: center;">Description</h4>
+          <p>
+            The Last of Us is an action-adventure survival horror video game developed by Naughty Dog and published by Sony Computer Entertainment. It was released for the PlayStation 3 worldwide on June 14, 2013. Players control Joel, a smuggler tasked with escorting a teenage girl named Ellie across a post-apocalyptic United States. The Last of Us is played from a third-person perspective; players use firearms and improvised weapons, and can use stealth to defend against hostile humans and cannibalistic creatures infected by a mutated strain of the Cordyceps fungus. In the game's online multiplayer mode, up to eight players engage in cooperative and competitive gameplay.
+          </p>
+          <p>
+            Development of The Last of Us began in 2009, soon after the release of Naughty Dog's previous game, Uncharted 2: Among Thieves. The relationship between Joel and Ellie became the central focus of the game, with all other elements developed around it. Actors Troy Baker and Ashley Johnson portrayed Joel and Ellie respectively through voice and motion capture, and assisted creative director Neil Druckmann with the development of the characters and story. The original score was composed and performed by Gustavo Santaolalla.
+          </p>
+          <p>
+            Following its announcement in December 2011, The Last of Us was widely anticipated. Upon release, it received universal acclaim, with praise directed at its narrative, gameplay, visual and sound design, characterization, and depiction of female characters. The Last of Us became one of the best-selling games on the PlayStation 3, selling over 1.3 million units in its first week and over eight million units within fourteen months. It won year-end accolades, including multiple Game of the Year awards from several gaming publications, critics, and game award ceremonies, making it one of the most awarded games in history, and is considered to be one of the greatest video games of all time.
+          </p>
+          <p>
+            After the game's release, Naughty Dog released several downloadable content additions; The Last of Us: Left Behind adds a single-player campaign following Ellie and her best friend Riley. An enhanced edition of the original game, The Last of Us Remastered, was released for the PlayStation 4 in July 2014.[a] A sequel, The Last of Us Part II, was announced in December 2016.
+
+          </p>
+        </div>
+
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+
+  <?php include('./Layouts/footer.php'); ?>
